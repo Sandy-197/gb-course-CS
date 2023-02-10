4968290bb4479b068199160704621d946f75136b
Alex <4276038@gmail.com>
2023-02-10 21:03:26 +0300

    Добавил комментарии к структуре if

--- homeworks/seminar-1/task-1/Program.cs
+++ homeworks/seminar-1/task-1/Program.cs
@@ -10,15 +10,15 @@ int second = int.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите третье число: ");
 int third = int.Parse(Console.ReadLine() ?? "0");
 
-if (first > second && first > third)
+if (first > second && first > third)    // Проверяем если первое число больше второго и третьего, значит оно самое большое
 { 
     Console.WriteLine("Максимальное число: " + first);
 }
-else if (second > third)
+else if (second > third)    // Уже знаем, что первое не самое большое, проверяем больше ли второе, чем третье, если да, то оно самое большое
 {
     Console.WriteLine("Максимальное число: " + second);
 }
-else 
+else  // Уже знаем, что первое и второе не самые большие, значит 3 самое большое. 
 {
     Console.WriteLine("Максимальное число: " + third);
 }
