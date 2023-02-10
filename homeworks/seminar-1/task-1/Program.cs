ab563fd9e51c9e0ceccbc3f6ec5644c2657bcde7
Alex <4276038@gmail.com>
2023-02-10 20:00:51 +0300

    Сделал новую конфигурацию папок

---
+++ homeworks/seminar-1/task-1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write ("Введите первое число: ");
+int first = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int second = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите третье число: ");
+int third = int.Parse(Console.ReadLine() ?? "0");
+
+if ((first > second) & (first > third)) 
+{ 
+    Console.WriteLine("Максимальное число: " + first);
+}
+else if (second > third)
+{
+    Console.WriteLine("Максимальное число: " + second);
+}
+else 
+{
+    Console.WriteLine("Максимальное число: " + third);
+}
