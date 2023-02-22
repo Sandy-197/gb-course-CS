ed35a3f9ace18a1cc4c02e927ac67fd366abab30
Alex <4276038@gmail.com>
2023-02-22 18:42:51 +0300

    решение скорректированное

--- homeworks/seminar-3/task-23/Program.cs
+++ homeworks/seminar-3/task-23/Program.cs
@@ -2,35 +2,46 @@
 // * при N < 0, нужно вывести от N до -1
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+string Prompt(string intro, bool oneline = true)
+{
+    Console.Write($"{intro}" + ((oneline) ? "" : "\n").ToString());
+    string res = Console.ReadLine()??"";
+    return res;
+}
 
-Console.Write("Введтьте число N:");
-int n = int.Parse(Console.ReadLine() ?? "0");
+Console.Clear();
+int n = int.Parse(Prompt("Введтьте число N:") ?? "0");
 int count = 1;
-
 if (n < 0)
 {
     count = n;
     n = -1;
 }
-// решение через массив
-Console.WriteLine("Решение через массив");
 
+int tempCount = count; // сохраняем count для следующего решения
+
+// решение через массив
+// мое решение, мне кажется проще чем у вас в 22 задаче на семинаре. 
+// по крайней мере нет в цикле создания массива и недопониманий почему индекс массива переменная вне цикла :)
+
+Console.WriteLine("Решение через массив");
 int maxIndex = Math.Abs(n - count + 1);
 int[] a = new int[maxIndex];
-int j = count; // только из-за того, что есть второе решение. а так монжо складывать count в 27 строке, и считать егоже в 26
-// мое решение, мне кажется проще чем у вас в 22 задаче на семинаре. 
-// по крайней мере нет в цикле создания массива недопониманий почему индекс массива переменная вне цикла :)
+
 for (int i = 0; i < maxIndex; i++)
 {
-    a[i] = Convert.ToInt32(Math.Pow(j,3));
-    j++;
-}
-for (int i = 0; i < maxIndex; i++)
-{
-    Console.Write($"{a[i]}" + ((i != maxIndex - 1) ? ", " : ".").ToString());
+    a[i] = Convert.ToInt32(Math.Pow(count,3));
+    count++;
 }
 
+for (int i = 0; i < maxIndex; i++)
+{
+    Console.Write($"{a[i]}" + ((i != maxIndex - 1) ? ", " : ".\n").ToString());
+}
+Console.WriteLine($"{String.Join(", ",a)}.");
+
 // решение через while, мне кажется в данном случае оно проще и понятнее чем через for, если речь только о выводе на экран.
+count = tempCount; //востанавливаем значения count для следующего решения.
 
 Console.WriteLine("\nРешение через while, мне кажется проще");
 while (count <= n) 
