a32803588a475f16ac79807edaaeeab9a369fd9e
Alex <4276038@gmail.com>
2023-02-22 18:23:49 +0300

    добавлено еще одно решение в задачу 19

--- homeworks/seminar-3/task-19/Program.cs
+++ homeworks/seminar-3/task-19/Program.cs
@@ -14,22 +14,32 @@ int Prompt(string intro, bool oneline = true)
 int CountDigitInInt(int n) // подсчет кол-ва разрядов в числе, через математику. Так же можно через длину строки. но не так интересно.
 {
     int count = 0;
-    while (n!=0)
-        {
-            count++;
-            n /= 10;
-        }
+    while (n != 0)
+    {
+        count++;
+        n /= 10;
+    }
     return count;
 }
-
-bool IsPolindrom(int p) // Решение через математику
+bool IsPalindrom(int p) // Решение через математику
+{
+    int t = p;
+    int revp = 0;
+    while (t > 0)
+    {
+        revp = revp*10 + t % 10;
+        t /= 10;
+    }
+    return (revp-p==0);
+}
+bool IsPolindrom(int p) // Еще одно решение через математику и логику.
 {
     int i = 0;
     int countDigit = CountDigitInInt(p);
     int coutnDigitHulf = countDigit / 2;
     while (i < coutnDigitHulf)
     {
-        if (Convert.ToInt32(p/Math.Pow(10,i))%10 != Convert.ToInt32(p/Math.Pow(10,countDigit-i-1))%10) { return false; }
+        if (Convert.ToInt32(p / Math.Pow(10, i)) % 10 != Convert.ToInt32(p / Math.Pow(10, countDigit - i - 1)) % 10) { return false; }
         i++;
     }
     return true;
@@ -37,10 +47,11 @@ bool IsPolindrom(int p) // Решение через математику
 
 bool IsPolindrom2(int p)  // Решение через конвертацию и разворачивание строки
 {
-    return ((Convert.ToInt32(string.Concat(p.ToString().Reverse().ToArray()))-p)==0);
+    return ((Convert.ToInt32(string.Concat(p.ToString().Reverse().ToArray())) - p) == 0);
 }
 
 int n = Prompt("Введите число: ");
 // if (n < 10000 || n > 99999) { Console.WriteLine("Вы ввели не 5 значное число."); return ; } // нужно только если хотим обрабатывать 5 значные
-Console.WriteLine($"Число {n} "+(IsPolindrom(n) ? "" : "не ").ToString()+"палиндром.");
-Console.WriteLine($"Число {n} "+(IsPolindrom2(n) ? "" : "не ").ToString()+"палиндром.");
+Console.WriteLine($"Число {n} " + (IsPalindrom(n) ? "" : "не ").ToString() + "палиндром.");
+Console.WriteLine($"Число {n} " + (IsPolindrom(n) ? "" : "не ").ToString() + "палиндром.");
+Console.WriteLine($"Число {n} " + (IsPolindrom2(n) ? "" : "не ").ToString() + "палиндром.");
