304386bd132d033178bdf61314ee5651f9be3966
Alex <4276038@gmail.com>
2023-03-06 23:09:13 +0300

    решение для задачи 50

--- homeworks/seminar-7/task-50/Program.cs
+++ homeworks/seminar-7/task-50/Program.cs
@@ -10,17 +10,19 @@
 
 using System;
 using static System.Console;
-int ROW = 0;
-int COLUMN = 1;
+const int ROW = 0;      // константа для использования в GetArray(), чтобы не путать когда строки когда столбцы.
+const int COLUMN = 1;   // константа для использования в GetArray(), чтобы не путать когда строки когда столбцы.
 
 Clear();
+// Инициализация массива m строк и n столбцов
 int m = int.Parse(Prompt("Введите количество строк массива: "));
 int n = int.Parse(Prompt("Введите количество строк массива: "));
-int r = int.Parse(Prompt("Введите номер строки массива для поиска: "));
-int c = int.Parse(Prompt("Введите номер колонки массива для поиска: "));
-
 int[,] array = GetArray(m, n, 10, 99);
 PrintArray(array);
+
+// Поиск элемента массива по индексу
+int r = int.Parse(Prompt("Введите номер строки массива для поиска: "));
+int c = int.Parse(Prompt("Введите номер колонки массива для поиска: "));
 int? value = GetValueFromArray(r, c, array);
 if (value != null)
 {
@@ -28,10 +30,28 @@ if (value != null)
 }
 else
 {
-    WriteLine($"Позиция [{r},{c}] отсутсвует. Индекс массива должен быть в пределах [0..{array.GetLength(ROW)-1},0..{array.GetLength(COLUMN)-1}]");
+    WriteLine($"Позиция [{r},{c}] отсутсвует. Индекс массива должен быть в пределах [0..{array.GetLength(ROW) - 1},0..{array.GetLength(COLUMN) - 1}]");
 }
 
+// Поиск нужного значения по элементам массива
+int f = int.Parse(Prompt("Введите икомое число: "));
+int[,] fArray = GetCoordinateOfValue(array, f);
+if (fArray.GetLength(ROW) == 0)
+{
+    WriteLine("Такого элемента нет.");
+}
+else
+{
+    PrintArray(fArray);
+}
 
+/* Методы */
+
+// Prompt выводит на экран строку приглашение intro. 
+// запрашивает ввод строки от пользователя.
+// параметр onelin:
+// если равен true, то строка приглашение и ввод идет в одной строке.
+// если равен false, то добавляется к приглашению перевод строки.
 string Prompt(string intro, bool oneline = true)
 {
     Console.Write($"{intro}" + ((oneline) ? "" : "\n").ToString());
@@ -39,6 +59,12 @@ string Prompt(string intro, bool oneline = true)
     return res;
 }
 
+// GetArray возвращае двумерный массив целых значений с заданынми параметрами.
+// Параметры:
+// row : целое число строк
+// columt : целое чиисло колонок
+// minValue : минимальное целое для случайных чисел, по умолчанию 0
+// maxValue : максимальное целое для случайных чисел, по умолчанию 0
 int[,] GetArray(int row, int column, int minValue = 0, int maxValue = 0)
 {
     int[,] result = new int[row, column];
@@ -55,19 +81,71 @@ int[,] GetArray(int row, int column, int minValue = 0, int maxValue = 0)
     return result;
 }
 
+// PrintArray - вывоит в консоль двумерный массив, переданный в параметре.
+// Параметры:
+// inArray - целочисленыый двумерный массив
 void PrintArray(int[,] inArray)
 {
-    for (int i = 0; i < inArray.GetLength(ROW); i++)
+    for (int r = 0; r < inArray.GetLength(ROW); r++)
     {
-        for (int j = 0; j < inArray.GetLength(COLUMN); j++)
+        for (int c = 0; c < inArray.GetLength(COLUMN); c++)
         {
-            Write($"{inArray[i, j]} ");
+            Write($"{inArray[r, c]} ");
         }
         WriteLine();
     }
 }
 
+// GetValueFromArray - возвращает значение эелемента массивы заданого параметрами, 
+// если позиции элемента отсутствуют, возвращается значение null
+// Параметры:
+// row - челочисленное номер строки элемента к возврату
+// column - челочисленное номер столбца эелемента к возврату
+// array - челочисленный двумерный массив
 int? GetValueFromArray(int row, int column, int[,] array)
 {
     return ((column < array.GetLength(COLUMN) && column >= 0) && (row < array.GetLength(ROW) && row >= 0)) ? array[row, column] : null;
+}
+
+// AddRowToArray - добавляет нужное колво строк к массиву согласо параметрам.
+// Параметры:
+// array - двумерный исходный массив
+// newRow - кол-во строк которые нужно доавить к массиву.
+// строки инициализируются нулями
+int[,] AddRowToArray(int[,] array, int newRow)
+{
+    int[,] result = new int[array.GetLength(ROW) + newRow, array.GetLength(COLUMN)];
+
+    for (int r = 0; r < array.GetLength(ROW); r++)
+    {
+        for (int c = 0; c < array.GetLength(COLUMN); c++)
+        {
+            result[r, c] = array[r, c];
+        }
+    }
+    return result;
+}
+
+// GetCoordinateOfValue - возвращает двумерный массив с координатами найденных элементов. 
+// Если не найдено ни одного элеента, то возвращает массив из 0 строк.
+// Параметры araay - двуменый целочисленный массив.
+// toFind - целоу число, которое надо найти в массиве
+int[,] GetCoordinateOfValue(int[,] array, int toFind)
+{
+    int[,] result = new int[0, 2];
+    int count = 0;
+    for (int r = 0; r < array.GetLength(0); r++)
+    {
+        for (int c = 0; c < array.GetLength(0); c++)
+        {
+            if (array[r, c] == toFind)
+            {
+                result = AddRowToArray(result, 1);
+                result[count, 0] = r;
+                result[count, 1] = c;
+                count++;
+            }
+        }
+    }
+    return result;
 }
