5a3b3c852a854fa1b1b22eab5d45b4ea820ce468
Alex <4276038@gmail.com>
2023-03-06 23:34:08 +0300

    испарвления в задаче 52

--- homeworks/seminar-7/task-52/Program.cs
+++ homeworks/seminar-7/task-52/Program.cs
@@ -10,10 +10,10 @@
 
 using System;
 using static System.Console;
-int ROW = 0;
-int COLUMN = 1;
-bool BY_COLUMN = true;
-bool BY_ROW = false;
+const int ROW = 0;
+const int COLUMN = 1;
+const bool BY_COLUMN = true;
+const bool BY_ROW = false;
 
 Clear();
 int m = int.Parse(Prompt("Введите количество строк массива: "));
@@ -23,6 +23,12 @@ int[,] array = GetArray(m, n, 10, 99);
 
 PrintTwoDemensionsArray(array);
 WriteLine();
+
+Write("Средние значения по столбцам: ");
+PrintArray(MeanValue(array, BY_COLUMN));
+WriteLine();
+
+Write("Средние значения по строкам: ");
 PrintArray(MeanValue(array, BY_ROW));
 
 /* Методы */
@@ -30,10 +36,7 @@ int[] MeanValue(int[,] array, bool byColumn = true)
 {
     int[] res;
     int i = 0;
-    // int count = 0;
-    // count = (byColumn) ? array.GetLength(COLUMN) : array.GetLength(ROW);
-    // res = new int[count];
-
+    
     res = (byColumn) ? new int[array.GetLength(COLUMN)] : new int[array.GetLength(ROW)];
     for (int r = 0; r < array.GetLength(ROW); r++)
     {
@@ -59,11 +62,11 @@ int[,] GetArray(int row, int column, int minValue = 0, int maxValue = 0)
     int[,] result = new int[row, column];
     if (!(minValue == 0 && maxValue == 0))
     {
-        for (int i = 0; i < m; i++)
+        for (int r = 0; r < m; r++)
         {
-            for (int j = 0; j < n; j++)
+            for (int c = 0; c < n; c++)
             {
-                result[i, j] = new Random().Next(minValue, maxValue + 1);
+                result[r, c] = new Random().Next(minValue, maxValue + 1);
             }
         }
     }
@@ -72,11 +75,11 @@ int[,] GetArray(int row, int column, int minValue = 0, int maxValue = 0)
 
 void PrintTwoDemensionsArray(int[,] inArray)
 {
-    for (int i = 0; i < inArray.GetLength(ROW); i++)
+    for (int r = 0; r < inArray.GetLength(ROW); r++)
     {
-        for (int j = 0; j < inArray.GetLength(COLUMN); j++)
+        for (int c = 0; c < inArray.GetLength(COLUMN); c++)
         {
-            Write($"{inArray[i, j]} ");
+            Write($"{inArray[r, c]}\t");
         }
         WriteLine();
     }
@@ -84,8 +87,8 @@ void PrintTwoDemensionsArray(int[,] inArray)
 
 void PrintArray(int[] inArray)
 {
-    for (int i = 0; i < inArray.Length; i++)
+    foreach  (int el in inArray)
     {
-        Write($"{inArray[i]} ");
+        Write($"{el} ");
     }
 }
